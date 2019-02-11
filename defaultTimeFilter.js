ff304ecdb3494dab3a0475590a823603a64e5088
Jimmy <jimmy.vancw@gmail.com>
2019-02-11 17:26:04 +0800

    Update defaultTimeFilter.js

--- defaultTimeFilter.js
+++ defaultTimeFilter.js
@@ -49,7 +49,7 @@
         // This uses the window.location.origin property to retrieve the scheme, hostname, and 
         // port where the parent extension is currently running, so this string doesn't have
         // to be updated if the extension is deployed to a new location.
-        const popupUrl = `${window.location.origin}/defaultTimeFilterDialog.html`;
+        const popupUrl = `${window.location.origin}/defaultTimeFilterDialog.html`;alert(popupUrl);
 
         let startMonth = getFiscalStartMonthSetting();
         if (startMonth == undefined) {
